036a47d58e21d5151b1522804e78b021c3b88701
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2023-06-05 17:09:12 +0100

    implement inference through http post

--- local.py
+++ local.py
@@ -1,10 +1,38 @@
-import IPython
 import soundfile as sf
 from tango import Tango
 
 tango = Tango("local", "cpu")
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-prompt = "An audience cheering and clapping"
-audio = tango.generate(prompt)
-sf.write(f"{prompt}.wav", audio, samplerate=16000)
-IPython.display.Audio(data=audio, rate=16000)
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_POST(self):
+        if (self.path == '/'):
+            content_length = int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length)
+            prompt = body.decode("utf-8")
+            audio = tango.generate(prompt)
+            sf.write(f"{prompt}.wav", audio, samplerate=16000)
+            self.send_response(200)
+            self.send_header("Content-type", "audio/wav")
+            self.end_headers()
+            self.wfile.write(audio)
+        else:
+          self.send_response(404)
+          self.end_headers()
+          self.wfile.write(bytes("Not found", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
