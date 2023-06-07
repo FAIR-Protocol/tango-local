2f25548a294ecbac16495b6270477bb7c8985ed1
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2023-06-07 11:34:20 +0100

    finish implement tango local server wrapper

--- local.py
+++ local.py
@@ -1,4 +1,6 @@
 import soundfile as sf
+from os import path
+import json
 from tango import Tango
 
 tango = Tango("local", "cpu")
@@ -6,7 +8,8 @@ tango = Tango("local", "cpu")
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 hostName = "localhost"
-serverPort = 8080
+serverPort = 8081
+currDir = path.dirname(path.abspath(__file__))
 
 class MyServer(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -14,12 +17,18 @@ class MyServer(BaseHTTPRequestHandler):
             content_length = int(self.headers['Content-Length'])
             body = self.rfile.read(content_length)
             prompt = body.decode("utf-8")
-            audio = tango.generate(prompt)
+            print(prompt)
+            audio = tango.generate(prompt, 10, 3, 1, False)
+            print("audio gfenerated. Saving...")
             sf.write(f"{prompt}.wav", audio, samplerate=16000)
+            file_path = path.join(currDir, f"{prompt}.wav")
+            json_data = {
+                "filqePath": file_path
+            }
             self.send_response(200)
-            self.send_header("Content-type", "audio/wav")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(audio)
+            self.wfile.write(bytes(json.dumps(json_data), "utf-8"))
         else:
           self.send_response(404)
           self.end_headers()
